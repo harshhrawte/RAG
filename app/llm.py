8e5ce38f79aab11e9bb7ae6a12194fd846efd088
Harsh Rawte <134953151+harshhrawte@users.noreply.github.com>
2025-05-21 02:37:30 +0530

    changed req

--- app/llm.py
+++ app/llm.py
@@ -1,6 +1,6 @@
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 from chromadb import PersistentClient
 from config import GROQ_API_KEY, HF_TOKEN  # Import both keys
 import streamlit as st
