a096eacb798a6f3f414aa54dd04911ac4ee9ca8b
Harsh Rawte <134953151+harshhrawte@users.noreply.github.com>
2025-05-21 13:26:03 +0530

    Changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 # app/__init__.py
 
+import sys
+import sqlite3
+sys.modules["sqlite3"] = sqlite3
+
 from config import GROQ_API_KEY, HF_TOKEN
 from .chroma_data_pipeline import build_chroma_db
 from .chatbot import answer_query
